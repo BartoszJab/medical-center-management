aed1d1baca1e14e0c2b5948b061d76c92be61725
BartoszJab <bartekj1221@wp.pl>
2023-03-20 19:54:47 +0100

    Initial commit

---
+++ src/main/java/com/example/medicalcentermanagement/MedicalCenterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalcentermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCenterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCenterManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicalcentermanagement/MedicalCenterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalcentermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
