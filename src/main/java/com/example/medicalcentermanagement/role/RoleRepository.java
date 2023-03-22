8a8aee1cb9f8f4a706589f1eb9247214e8eec97e
BartoszJab <bartekj1221@wp.pl>
2023-03-22 20:55:57 +0100

    Annotate RoleRepository with @Repository

--- src/main/java/com/example/medicalcentermanagement/role/RoleRepository.java
+++ src/main/java/com/example/medicalcentermanagement/role/RoleRepository.java
@@ -1,6 +1,8 @@
 package com.example.medicalcentermanagement.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
