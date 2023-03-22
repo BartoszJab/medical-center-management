cbb14aba3b6eaf884270acbeae0b047d84929cc2
BartoszJab <bartekj1221@wp.pl>
2023-03-22 12:37:45 +0100

    Create Role entity

---
+++ src/main/java/com/example/medicalcentermanagement/role/Role.java
@@ -0,0 +1,33 @@
+package com.example.medicalcentermanagement.role;
+
+import com.example.medicalcentermanagement.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleType;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+    )
+    private List<User> users;
+}
---
+++ src/main/java/com/example/medicalcentermanagement/role/RoleEnum.java
@@ -0,0 +1,5 @@
+package com.example.medicalcentermanagement.role;
+
+public enum RoleEnum {
+    PATIENT, EMPLOYEE, ADMIN
+}
